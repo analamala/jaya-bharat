7cc1edfab518597a13ccdfddbefa135846024535
analamala <nalamala_ajay@hotmail.com>
2023-11-12 05:56:51 -0500

    Add eqp retriever and electric dozer to products page. Cleanup code

--- src/components/Products/ProductsDetail.vue
+++ src/components/Products/ProductsDetail.vue
@@ -2,9 +2,15 @@
   <SAGESDetail class="mb-4"/>
   <v-divider></v-divider>
   <FeederBreakerDetail class="mt-2"/>
+  <v-divider></v-divider>
+  <EquipmentRetrieverDetail class="mt-2"/>
+  <v-divider></v-divider>
+  <ElectricDozerDetail class="mt-2"/>
 </template>
 
 <script setup lang="ts">
 import SAGESDetail from "@/components/Products/SAGESDetail.vue";
 import FeederBreakerDetail from "@/components/Products/FeederBreakerDetail.vue";
+import EquipmentRetrieverDetail from "@/components/Products/EquipmentRetrieverDetail.vue";
+import ElectricDozerDetail from "@/components/Products/ElectricDozerDetail.vue";
 </script>
